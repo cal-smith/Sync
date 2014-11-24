38339b0ad7352d84c8469d7de037665baad9abed
Callum Smith <callum1234moo@gmail.com>
2014-11-24 17:17:46 -0500

    improved the demo

--- app.js
+++ app.js
@@ -10,19 +10,44 @@ app.get('/', function(req, res) {
 app.use(express.static(__dirname + '/static'));
 
 var media;
+var queue = []
 io.on('connection', function (socket) {
 	if (media) {
 		socket.emit('sync', media);
 	}
 	socket.on('sync', function (data){
+		data.playing = 0;
+		data.duration = null;
+		data.played = 0;
+		queue.push(data);
+		media = data;
 		socket.emit('sync', data);
 		socket.broadcast.emit('sync', data);
-		media = data;
 	});
-	socket.on('time', function (data){
-		socket.broadcast.emit('time', data);
+
+	socket.on('play', function (data){
+		media.playing += 1;
+		if (media.playing == 1) {
+			play(data);
+		}
 	});
 });
+
+function play(data) {
+	media.duration = data.duration;
+	playback = setInterval(function(){
+		io.emit('time', {time: media.played});
+		media.played += 1;
+		status();
+	}, 1000);
+	function status (){
+		if (media.played >= media.duration) {
+			clearInterval(playback);
+			io.emit('done');
+			media = null;//media = next in queue?
+		}
+	}
+}
 
 var port = 5000;
 var ip = '127.0.0.1';
--- static/main.js
+++ static/main.js
@@ -1,6 +1,6 @@
 'use strict';
 var sync = io.connect(window.location.origin);
-var master = false;
+var played = 0;
 function start () {
 	sync.on('news', function (data){
 		console.log(data);
@@ -8,15 +8,16 @@ function start () {
 
 	sync.on('sync', function (data){
 		console.log(data);
-		var params = { allowScriptAccess: "always" };
-		var attr = { id: "youtube" };
+		if (typeof data.played !== 'undefined') { played = data.played; }
+		var params = { allowScriptAccess: 'always' };
+		var attr = { id: 'youtube' };
 		var url = 'https://youtube.com/v/'+data.url+'?enablejsapi=1&version=3&playerapiid=ytplayer';
 		swfobject.embedSWF(url, 'media', 640, 480, '8', null, null, params, attr);
 	});
 
 	sync.on('time', function(data){
-		console.log('time', data.time);
-		if (player) {
+		console.log(data.time)
+		if (typeof player !== 'undefined') {
 			if (player.getCurrentTime()-5 > data.time || player.getCurrentTime()+5 < data.time) {
 				console.log('attempting to sync');
 				player.seekTo(data.time, false);
@@ -24,13 +25,17 @@ function start () {
 		}
 	});
 
+	sync.on('done', function(){
+		played = 0;
+		player.stopVideo();
+	});
+
 	e("send").addEventListener('submit', function(event){
 		event.stopPropagation();
 		event.preventDefault();
 		var url = e('url').value;
 		//send(url);
-		master = true;
-		sync.emit('sync', {url:url});
+		sync.emit('sync', {url:url, type:'youtube'});
 		e('url').value = "";
 	});
 
@@ -49,16 +54,13 @@ function onYouTubePlayerReady(playerId){
 	console.log(playerId)
 	var player = e('youtube');
 	window.player = player;
-}
-
-window.setInterval(function(){
-	if (typeof player !== 'undefined') {
+	if (played > 0) {
+		player.seekTo(data.time, true);
+	} else {
+		sync.emit('play', {duration:player.getDuration()});
 		player.playVideo();
-		if (master) {
-			sync.emit('time', {time:player.getCurrentTime()});
-		}
 	}
-}, 500);
+}
 
 //kick off the party when the DOM is ready.
 document.addEventListener("DOMContentLoaded", function(event) {
