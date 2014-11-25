a17319ea60beba2988e6be5c68d86db5e5ec0576
Callum Smith <callum1234moo@gmail.com>
2014-11-25 01:34:39 -0500

    added soundcloud to the demo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var url = require('url');
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -9,20 +10,46 @@ app.get('/', function(req, res) {
 
 app.use(express.static(__dirname + '/static'));
 
-var media;
+var media = {playing:0};
 var queue = []
 io.on('connection', function (socket) {
 	if (media) {
+		console.log(media.played)
+		media.delta = Date.now();
 		socket.emit('sync', media);
+		socket.emit('queue', queue);
 	}
 	socket.on('sync', function (data){
+		data.url = url.parse(data.url, true);
+		if ((data.url.host == null || data.url.host == '') || data.url.protocol.indexOf('http') == -1) {
+			console.log('fail');
+			socket.emit('errors', {error: 'bad url'});
+			return;
+		}
+		console.log(media);
 		data.playing = 0;
 		data.duration = null;
 		data.played = 0;
+		console.log(data.url.host);
+		if (data.url.host.indexOf('youtube') !== -1) {
+			data.type = 'youtube';
+			data.url = data.url.query.v;
+		}else if (data.url.host.indexOf('soundcloud') !== -1) {
+			data.type = 'soundcloud';
+			data.url = data.url.href;
+		}
 		queue.push(data);
-		media = data;
-		socket.emit('sync', data);
-		socket.broadcast.emit('sync', data);
+		if (queue.length == 1 && media.playing == 0) {
+			io.emit('sync', data);
+			socket.broadcast.emit('sync', data);
+			if (media.playing == 0) {
+				media = queue.pop();
+			}
+		} else if (media.playing == 0){
+			media = queue.pop();
+		}
+		socket.emit('queue', queue);
+		socket.broadcast.emit('queue', queue);
 	});
 
 	socket.on('play', function (data){
@@ -36,7 +63,7 @@ io.on('connection', function (socket) {
 function play(data) {
 	media.duration = data.duration;
 	playback = setInterval(function(){
-		io.emit('time', {time: media.played});
+		io.emit('time', {time: media.played, type:data.type, delta:Date.now()});
 		media.played += 1;
 		status();
 	}, 1000);
@@ -44,7 +71,14 @@ function play(data) {
 		if (media.played >= media.duration) {
 			clearInterval(playback);
 			io.emit('done');
-			media = null;//media = next in queue?
+			if (queue.length > 0) {
+				media = queue.pop();
+				io.emit('queue', queue);
+				io.emit('sync', media);
+			} else {
+				media = {playing:0};
+				io.emit('queue', queue);
+			}
 		}
 	}
 }
--- static/main.js
+++ static/main.js
@@ -1,4 +1,7 @@
 'use strict';
+SC.initialize({
+	client_id: "09d14c4e812e7b9f44d72e45b7c86725"
+});
 var sync = io.connect(window.location.origin);
 var played = 0;
 function start () {
@@ -8,40 +11,78 @@ function start () {
 
 	sync.on('sync', function (data){
 		console.log(data);
+		window.data = data;
 		if (typeof data.played !== 'undefined') { played = data.played; }
-		var params = { allowScriptAccess: 'always' };
-		var attr = { id: 'youtube' };
-		var url = 'https://youtube.com/v/'+data.url+'?enablejsapi=1&version=3&playerapiid=ytplayer';
-		swfobject.embedSWF(url, 'media', 640, 480, '8', null, null, params, attr);
+		if (data.type == 'youtube') {
+			var params = { allowScriptAccess: 'always' };
+			var attr = { id: 'youtube' };
+			var url = 'https://youtube.com/v/'+data.url+'?enablejsapi=1&version=3&playerapiid=youtube';
+			swfobject.embedSWF(url, 'youtube', 640, 480, '8', null, null, params, attr);
+		}
+		if (data.type == 'soundcloud') {
+			SC.get('/resolve', { url: data.url }, function(track) {
+				console.log(track);
+				SC.stream(track.uri, function(sound){
+					soundcloud(sound, track);
+				});
+			});
+		}
 	});
 
 	sync.on('time', function(data){
-		console.log(data.time)
-		if (typeof player !== 'undefined') {
+		console.log(data.time*1000)
+		if (data.type == 'youtube' && typeof player !== 'undefined') {
 			if (player.getCurrentTime()-5 > data.time || player.getCurrentTime()+5 < data.time) {
 				console.log('attempting to sync');
 				player.seekTo(data.time, false);
+			}
+		}
+		if (data.type == 'soundcloud' && typeof sound !== 'undefined') {
+			if (sound.position-5000 > data.time*1000 || sound.position+5000 < data.time*1000) {
+				sound.setPosition(data.time*1000 + (Date.now() - data.delta));
+				console.log('attempting to sync', sound.position, data.time*1000 + (Date.now() - data.delta));
 			}
 		}
 	});
 
 	sync.on('done', function(){
 		played = 0;
-		player.stopVideo();
+		console.log('done');
+		var done = window.setInterval( function(){
+			if (data.type == 'youtube' && player.getCurrentTime() >= player.getDuration()){
+				e('youtube').remove();
+				e('media').innerHTML += '<div id="youtube"></div>';
+				//player.stopVideo();
+				stop();
+			}
+			if (data.type == 'soundcloud' && sound.position >= sound.duration) {
+				//delete sound manager stuff
+			}
+		}, 500);
+		function stop(){
+			window.clearInterval(done);
+		}
+	});
+
+	sync.on('queue', function(data){
+		e('queue').textContent = JSON.stringify({data:data}) + JSON.stringify(window.data);
+		console.log(data);
+	});
+
+	sync.on('errors', function(data){
+		console.log(data);
 	});
 
 	e("send").addEventListener('submit', function(event){
 		event.stopPropagation();
 		event.preventDefault();
 		var url = e('url').value;
-		//send(url);
-		sync.emit('sync', {url:url, type:'youtube'});
+		sync.emit('sync', {url:url});
 		e('url').value = "";
 	});
 
 	function send(url){
 		sync.emit('sync', {url:url});
-		//e('media').textContent = url;
 	}
 	window.sync = sync;
 }
@@ -51,15 +92,28 @@ function e(elem){
 }
 
 function onYouTubePlayerReady(playerId){
-	console.log(playerId)
 	var player = e('youtube');
 	window.player = player;
 	if (played > 0) {
 		player.seekTo(data.time, true);
 	} else {
-		sync.emit('play', {duration:player.getDuration()});
+		sync.emit('play', {duration:player.getDuration(), type:'youtube'});
 		player.playVideo();
 	}
+}
+
+function soundcloud (sound, track){
+	console.log(track, track.artwork_url.replace('large', 'original'), track.user.username, track.user.avatar_url.replace('large', 'original'));
+	//sound.load();
+	sync.emit('play', {duration:track.duration/1000, type:'soundcloud'});
+	if (played > 0) {
+		sound.play();
+		sound.setPosition((Date.now() - data.delta) + (data.played * 1000));
+	} else {
+		sound.play();
+	}
+	window.sound = sound;
+	window.track = track;
 }
 
 //kick off the party when the DOM is ready.
